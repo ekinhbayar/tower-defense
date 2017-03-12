383f73d5c3f7b597e0ffff04a89220f7d3dd86d0
Pieter Hordijk <info@pieterhordijk.com>
2017-03-12 21:30:16 +0100

    async is hard ya know

--- public/js/lib/UserInterface/Path.js
+++ public/js/lib/UserInterface/Path.js
@@ -7,16 +7,24 @@ TowerDefense.UserInterface.Path = (function() {
     function Path(x, y) {
         this.x = x;
         this.y = y;
+
+        this.loaded = false;
+
+        this.tile = new Image();
+
+        this.tile.addEventListener('load', () => {
+            this.loaded = true;
+        });
+
+        this.tile.src = 'img/road.jpg';
     }
 
     Path.prototype.draw = function(canvas) {
-        const tile = new Image();
+        if (!this.loaded) {
+            return;
+        }
 
-        tile.addEventListener('load', () => {
-            canvas.context.drawImage(tile, (this.x + canvas.x) * canvas.scale.x, (this.y + canvas.y) * canvas.scale.y, 20 * canvas.scale.x, 20 * canvas.scale.y);
-        });
-
-        tile.src = 'img/road.jpg';
+        canvas.context.drawImage(this.tile, (this.x + canvas.x) * canvas.scale.x, (this.y + canvas.y) * canvas.scale.y, 20 * canvas.scale.x, 20 * canvas.scale.y);
     };
 
     return Path;
