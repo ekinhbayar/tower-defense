df8943a495ceb9e009f8c90933cafd581a3e21b8
Pieter Hordijk <info@pieterhordijk.com>
2017-03-12 18:22:47 +0100

    Only update the FPS counter every 0.5 seconds

--- public/js/lib/Fps.js
+++ public/js/lib/Fps.js
@@ -4,18 +4,22 @@ TowerDefense.Fps = (function() {
     'use strict';
 
     function Fps() {
-        this.lastTime = +new Date();
-        this.fps      = 0;
+        this.lastUpdate = +new Date();
+        this.lastTime   = +new Date();
+        this.fps        = 0;
     }
 
     Fps.prototype.update = function(time) {
-        if (typeof time === 'undefined') {
+        if (typeof time === 'undefined' || Math.abs(time - this.lastUpdate) < 500) {
+            this.lastTime = time;
+
             return;
         }
 
         this.fps = 1000 / (time - this.lastTime);
 
         this.lastTime = time;
+        this.lastUpdate = time;
     };
 
     Fps.prototype.get = function() {
