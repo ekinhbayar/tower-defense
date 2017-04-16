cd33b481d7bb42e2b383c400840c505b8fb2385f
Pieter Hordijk <info@pieterhordijk.com>
2017-04-16 14:57:00 +0200

    Added map container

---
+++ src/Game/Map/Map.php
@@ -0,0 +1,64 @@
+<?php declare(strict_types = 1);
+
+namespace Game\Map;
+
+use Game\Entity\Coordinates;
+
+class Map
+{
+    private $spawnPoint;
+
+    private $players;
+
+    private $paths;
+
+    public function __construct(Coordinates $spawnPoint, array $players, array $paths)
+    {
+        $this->spawnPoint = $spawnPoint;
+        $this->players    = $players;
+        $this->paths      = $paths;
+    }
+
+    public function getAsArray(): array
+    {
+        return [
+            'spawn'  => $this->spawnPoint->getAsArray(),
+            'player' => [
+                'id'    => 1,
+                'token' => 'ewiy4734y7ed',
+            ],
+            'players' => $this->getPlayersAsArray(),
+            'path'    => $this->getPathAsArray(),
+        ];
+    }
+
+    private function getPlayersAsArray(): array
+    {
+        $players = [];
+
+        foreach ($this->players as $index => $player) {
+            $players[] = [
+                'id'   => $index + 1,
+                'name' => 'Player ' . $index,
+                'x'    => $player->getX(),
+                'y'    => $player->getY(),
+            ];
+        }
+
+        return $players;
+    }
+
+    private function getPathAsArray(): array
+    {
+        $path = [];
+
+        foreach ($this->paths as $path) {
+            $path[] = [
+                'x' => $path->getPositionX(),
+                'y' => $path->getPositionY(),
+            ];
+        }
+
+        return $path;
+    }
+}
