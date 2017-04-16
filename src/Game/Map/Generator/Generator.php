88d9ba2f60c03e17ea2332c3063d08aecf295b62
Pieter Hordijk <info@pieterhordijk.com>
2017-04-16 15:36:14 +0200

    Generator now returns a map

--- src/Game/Map/Generator/Generator.php
+++ src/Game/Map/Generator/Generator.php
@@ -2,12 +2,16 @@
 
 namespace Game\Map\Generator;
 
+use Game\Map\Map;
+
 class Generator
 {
-    public function generate(int $width, int $height, int $tileSize, int $numberOfPlayer)
+    public function generate(int $width, int $height, int $tileSize, int $numberOfPlayer): Map
     {
         $spawnPoint = (new Spawn($width, $height))->generate();
         $players    = (new Players(new Player($width, $height)))->generate($numberOfPlayer);
         $paths      = (new Path($width, $height, $tileSize))->generate($players, $spawnPoint);
+
+        return new Map($spawnPoint, $players, $paths);
     }
 }
